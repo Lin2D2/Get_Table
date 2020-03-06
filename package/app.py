527d7da6dea88ea637268b8f72722caf6720e7e9
Lin2D2 <linus@thebehrens.net>
2020-03-06 23:04:34 +0100

    smaller tweaks

--- package/app.py
+++ package/app.py
@@ -21,7 +21,8 @@ logging_time = LoggingTime(logging.INFO)
 
 class App:
     def __init__(self):
-        logging.basicConfig(level=logging.DEBUG)
+        logging_time.info("Start")
+        time.sleep(0.01)
         username = input("enter the username: ")
         password = input("enter the password: ")
         self.table_object = TableUtil({'_username': username, '_password': password})
@@ -38,10 +39,7 @@ class App:
     def update(self):
         logging_time.info("performing update...")
         self.table_object.update()
-        start_time = time.time()
         threading.Thread(target=self.write_file, args=[self.table_object], name="file_write").start()
-        end_time = time.time()
-        logging_time.info("time taken to write file: " + str(end_time - start_time))
 
     def timing(self, timing=None):
 
@@ -71,6 +69,7 @@ class App:
                     smaller.append({"index": real_timings.index(real_timing), "difference": real_timing - real_now})
             if real_now - real_last_update >= 10 * 60:
                 if len(smaller) == 0:
+                    # TODO do a better solution...
                     logging_time.info("sleeping 100 sec")
                     time.sleep(100)
                 else:
@@ -88,6 +87,8 @@ class App:
             check_time()
 
     def write_file(self, object_of_table):
+        start_time = time.time()
+
         def create_changes_structur(status, massage=None, content=None):
             if not massage and not content:
                 return {
@@ -143,7 +144,7 @@ class App:
                                                    object_of_table.content_tomorow)
 
         def find_changes_in_table(old_table, new_table):
-            logging_time.info("looking for changes in table")
+            logging_time.info("looking for changes in table", "file_write_thread")
             unique_id = old_table[-1]["id"] + 1
 
             def extract_row(row):
@@ -162,15 +163,15 @@ class App:
             for e in old_table:
                 if not e["row"] in extract_row(new_table):
                     subtractions.append(e)
-            logging_time.debug(["additions: ", additions])
-            logging_time.debug(["subtractions: ", subtractions])
+            logging_time.debug(["additions: ", additions], "file_write_thread")
+            logging_time.debug(["subtractions: ", subtractions], "file_write_thread")
             return {"additions": additions, "subtractions": subtractions}
 
         def add_changes_to_day(day, table):
-            logging_time.debug(["day: ", day])
-            logging_time.debug(["table: ", table])
+            logging_time.debug(["day: ", day], "file_write_thread")
+            logging_time.debug(["table: ", table], "file_write_thread")
             if not day["inital_content"] == table["inital_content"]:
-                logging_time.debug(["add_changes_to_day day =", day])
+                logging_time.debug(["add_changes_to_day day =", day], "file_write_thread")
                 day["latest_status"] = table["latest_status"]
                 massage_changed = None
                 content_changed = None
@@ -181,7 +182,7 @@ class App:
                                                             table["inital_content"]["content"])
                 return create_changes_structur(table["latest_status"], massage_changed, content_changed)
             else:
-                logging_time.info("nothing changed")
+                logging_time.info("nothing changed", "file_write_thread")
                 return create_changes_structur(table["latest_status"])
 
         def calc_file(table_item, date_of_table):
@@ -204,7 +205,7 @@ class App:
                                         changes = add_changes_to_day(table_object_json, table_item)
                                         table_object_json["changes"].append(changes)
                                     else:
-                                        logging_time.info("The Status didn't changed")
+                                        logging_time.info("The Status didn't changed", "file_write_thread")
 
                             if not found_day:
                                 self.vertretungsplan_json[
@@ -236,11 +237,11 @@ class App:
             calc_file(table_item_today, extract_date_of_table(object_of_table.title_today))
             calc_file(table_item_tomorow, extract_date_of_table(object_of_table.title_tomorow))
         except AttributeError:
-            logging_time.warning("failed to calculate and construct json file")
+            logging_time.warning("failed to calculate and construct json file", "file_write_thread")
 
         try:
             with open(self.save_location, "w") as file:
-                logging_time.info("writing file...")
+                logging_time.info("writing file...", "file_write_thread")
                 json.dump(self.vertretungsplan_json, file, indent=2)
 
         except FileNotFoundError or AttributeError:
@@ -249,7 +250,7 @@ class App:
             except ValueError:
                 pass
             with open(self.save_location, "w+") as file:
-                logging_time.info("rewriting file")
+                logging_time.info("rewriting file", "file_write_thread")
                 json.dump([
                     {"year": self.now.year, "months":
                         [{"month": self.now.month, "days": [
@@ -257,6 +258,9 @@ class App:
                         ]}
                          ]}
                 ], file, indent=2)
+
+        end_time = time.time()
+        logging_time.info("time taken to write file: " + str(end_time - start_time), "file_write_thread")
 
 
 class TableUtil:
--- package/util/logging_time.py
+++ package/util/logging_time.py
@@ -7,13 +7,25 @@ class LoggingTime:
         logging.basicConfig(level=level)
 
     @staticmethod
-    def info(input_stuff):
-        return logging.info(":logged at:[" + str(datetime.datetime.now().ctime()) + "]:: " + str(input_stuff))
+    def info(input_stuff, thread=None):
+        if not thread:
+            return logging.info(":logged at:[" + str(datetime.datetime.now().ctime()) + "]:: " + str(input_stuff))
+        else:
+            return logging.info(":logged at:[" + str(datetime.datetime.now().ctime())
+                                + "]:in \"" + thread + "\":: " + str(input_stuff))
 
     @staticmethod
-    def debug(input_stuff):
-        return logging.debug(":logged at:[" + str(datetime.datetime.now().ctime()) + "]:: " + str(input_stuff))
+    def debug(input_stuff, thread=None):
+        if not thread:
+            return logging.debug(":logged at:[" + str(datetime.datetime.now().ctime()) + "]:: " + str(input_stuff))
+        else:
+            return logging.debug(":logged at:[" + str(datetime.datetime.now().ctime())
+                                 + "]:in \"" + thread + "\":: " + str(input_stuff))
 
     @staticmethod
-    def warning(input_stuff):
-        return logging.warning(":logged at:[" + str(datetime.datetime.now().ctime()) + "]:: " + str(input_stuff))
+    def warning(input_stuff, thread=None):
+        if not thread:
+            return logging.warning(":logged at:[" + str(datetime.datetime.now().ctime()) + "]:: " + str(input_stuff))
+        else:
+            return logging.warning(":logged at:[" + str(datetime.datetime.now().ctime())
+                                   + "]:in \"" + thread + "\":: " + str(input_stuff))
