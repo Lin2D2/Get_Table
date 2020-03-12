64126b8d102d66b8af638ffafb2adcc56482b0af
space <linus@thebehrens.net>
2020-03-12 13:54:05 +0100

    better solution for the logging with time thing

--- package/app.py
+++ package/app.py
@@ -15,10 +15,26 @@ try:
 except ImportError:
     sys.exit("failed because bs4 is not installed, do pip install bs4 to fix this")
 
-from package.util.logging_time import LoggingTime
+
 from web_interface import app as web_app
 
-logging_time = LoggingTime(logging.INFO)
+# create logger
+logging_time = logging.getLogger("main")
+logging_time.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter
+formatter = logging.Formatter("%(asctime)s::%(levelname)s::%(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logging_time.addHandler(ch)
 
 
 class App:
