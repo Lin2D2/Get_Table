9de575ca53fa150a03f21e3978ea0b6fe961c2b0
space <linus@thebehrens.net>
2020-03-17 14:23:22 +0100

    joining another column to save space

--- package/util/calc_proper_table.py
+++ package/util/calc_proper_table.py
@@ -5,6 +5,7 @@ def calc(table_dont_change, changed_view=False):
     table = copy.deepcopy(table_dont_change)
     NDruck_was = []
     header_row = table["inital_content"]["header"]["row"]
+    index_of_VertretungsText = header_row.index("Vertretungs-Text")
     index_of_Mitbetreuung = header_row.index("Mitbetreuung")
     index_of_Entfall = header_row.index("Entfall")
     index_of_NDruck = header_row.index("N.Druck. (N)")
@@ -39,17 +40,21 @@ def calc(table_dont_change, changed_view=False):
 
     # joining Entfall and Mitbetreuung
     for row in table["inital_content"]["content"]:
+        value_of_VertretungsText = row["row"][index_of_VertretungsText]
         value_of_Mitbetreuung = row["row"][index_of_Mitbetreuung]
         value_of_Entfall = row["row"][index_of_Entfall]
-        del row["row"][index_of_Entfall]
-        if value_of_Entfall and value_of_Mitbetreuung == "x":
-            row["row"][index_of_Mitbetreuung] = "Entf. and Mitbe.???"
-        elif value_of_Entfall == "x":
-            row["row"][index_of_Mitbetreuung] = "Entf."
+        if value_of_Entfall == "x":
+            row["row"][index_of_Mitbetreuung] = "Entfall"
         elif value_of_Mitbetreuung == "x":
-            row["row"][index_of_Mitbetreuung] = "Mitbe."
+            row["row"][index_of_Mitbetreuung] = "Mitbetreuung"
+        elif value_of_VertretungsText != "\u00a0":
+            row["row"][index_of_Mitbetreuung] = value_of_VertretungsText
+    for row in table["inital_content"]["content"]:
+        del row["row"][index_of_Entfall]
+        del row["row"][index_of_VertretungsText]
+    table["inital_content"]["header"]["row"][index_of_Mitbetreuung] = "Info"
     del table["inital_content"]["header"]["row"][index_of_Entfall]
-    table["inital_content"]["header"]["row"][index_of_Mitbetreuung] = "Entf. and Mitbe."
+    del table["inital_content"]["header"]["row"][index_of_VertretungsText]
     # TODO merge place holder lines to one
     return table
 
