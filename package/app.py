8e43750673b9feb406472fb736562fa0bb9df311
Lin2D2 <linus@thebehrens.net>
2020-10-02 23:56:42 +0200

    Revert "merge"
    This reverts commit 696677d7e54fb8d7721515379d22fc101b1e4079, reversing
    changes made to c9f164692a652c8a35ce77559d186009f0ab7e0c.

--- package/app.py
+++ package/app.py
@@ -53,7 +53,8 @@ class App:
 
     def start_web_interface(self):
         logging_time.info("starting web-thread...")
-        web_app.run(data=None, parent=self, url="0.0.0.0", port="5000")
+        ip_adress = "192.168.56.1"
+        web_app.run(data=None, parent=self, url=ip_adress, port="5000")
 
     def update(self):
         logging_time.info("performing update...")
