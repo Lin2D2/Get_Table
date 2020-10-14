4430f73ea837303ea3667f8b6657983b9f3c025f
Lin2D2 <linus@thebehrens.net>
2020-10-14 22:23:21 +0200

    client now sends timetable to sever and back in the login if its exist on the sever

--- web_interface/app.py
+++ web_interface/app.py
@@ -15,7 +15,7 @@ except ImportError:
 week_days_german = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
 
 
-def routes(app, cors, data, parent):
+def routes(app, cors, parent):
     @app.route("/api/days", methods=['GET'])
     def table_view():
         days = []
@@ -65,6 +65,7 @@ def routes(app, cors, data, parent):
                                 "header": [header],
                                 "content": content,
                                 }
+        # TODO check for the weekend not with else
         else:
             print("weekend?")
             table_today_data = {}  # temp
@@ -81,15 +82,22 @@ def routes(app, cors, data, parent):
     def login():
         data = flask.request.data
         # {username: <name>, password: <passwort>}
+        # parent.database_users.insert({"username": "root", "password": "12345678"})
         # TODO for adding users: parent.database_users.insert(data)
         search = parent.database_users.search(tinydb.where("username") == data["username"])
         if 2 > len(search) > 0:
             search = search[0]
             print(search)
             if search["password"] == data["password"]:
-                json_resp = json.dumps({
-                    "state": "success",
-                })
+                try:
+                    json_resp = json.dumps({
+                        "state": "success",
+                        "timetable": search["timetable"],
+                    })
+                except KeyError:
+                    json_resp = json.dumps({
+                        "state": "success",
+                    })
                 # TODO add some cockie or key that this cant be faked
             else:
                 json_resp = json.dumps({
@@ -109,12 +117,42 @@ def routes(app, cors, data, parent):
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return resp
 
+    @app.route("/api/timetable/push", methods=["POST"])
+    def post_timetable():
+        # TODO handle it over cookies don't send password twice
+        data = flask.request.data
+        User = tinydb.Query()
+        search = parent.database_users.search(User.username == data["login"]["username"])
+        if 2 > len(search) > 0:
+            search = search[0]
+            if search["password"] == data["login"]["password"]:
+                parent.database_users.update(
+                    {"timetable": {"monday": data["monday"], "tuesday": data["tuesday"], "wednesday": data["wednesday"],
+                                   "thursday": data["thursday"], "friday": data["friday"]}},
+                    User.username == data["login"]["username"])
+                json_resp = json.dumps({
+                    "state": "success",
+                })
+            else:
+                json_resp = json.dumps({
+                    "state": "failed login",
+                })
+        else:
+            print("something not expected!!!")
+            print(data)
+            json_resp = json.dumps({
+                "state": "failed",
+            })
+        resp = flask.Response(json_resp, content_type='application/json; charset=utf-8')
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
 
-def run(data, parent, url, port):
+
+def run(parent, url, port):
     app = FlaskAPI(__name__)
     cors = CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
-    routes(app, cors, data, parent)
+    routes(app, cors, parent)
     app.run(host=url, port=port, threaded=True, use_reloader=False, debug=True)
 #     app.run(debug=True, host=url, port=port, threaded=True, use_reloader=True)
 
